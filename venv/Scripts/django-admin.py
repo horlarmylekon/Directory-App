6691b48f5aa64a4ac351b57da01fe7f29f536d5f
intelliSenseCoder <40922213+intelliSenseCoder@users.noreply.github.com>
2019-07-08 04:05:04 +0100

    Initial commit

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\intelisense\desktop\django project\directoryapp\venv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
